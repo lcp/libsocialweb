94b96b207a034c505fcd5155c4ef602d62ff8760
Ross Burton <ross@linux.intel.com>
2008-11-27 11:55:10 +0000

    Start mojito in the monitor

--- examples/monitor.py
+++ examples/monitor.py
@@ -6,6 +6,7 @@ from dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
 
 bus = dbus.SessionBus()
+bus.start_service_by_name("com.intel.Mojito")
 
 mojito = bus.get_object("com.intel.Mojito", "/com/intel/Mojito")
 mojito = dbus.Interface(mojito, "com.intel.Mojito")
