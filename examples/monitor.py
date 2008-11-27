85ff425513c27191309c49bd78b2b94c59603349
Ross Burton <ross@linux.intel.com>
2008-11-27 12:25:25 +0000

    Create a view and start it

--- examples/monitor.py
+++ examples/monitor.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import dbus
+import dbus, gobject
 from dbus.mainloop.glib import DBusGMainLoop
 
 DBusGMainLoop(set_as_default=True)
@@ -11,4 +11,15 @@ bus.start_service_by_name("com.intel.Mojito")
 mojito = bus.get_object("com.intel.Mojito", "/com/intel/Mojito")
 mojito = dbus.Interface(mojito, "com.intel.Mojito")
 
-print mojito.getSources()
+path = mojito.openView(mojito.getSources())
+view = bus.get_object("com.intel.Mojito", path)
+view = dbus.Interface(view, "com.intel.Mojito.View")
+
+def added(item, data):
+    print item, data
+
+view.connect_to_signal("ItemAdded", added)
+view.start()
+
+loop = gobject.MainLoop()
+loop.run()
