3ca3437cf9b1fee07c819f2917a6d381f09cfe47
Ross Burton <ross@linux.intel.com>
2008-11-27 17:27:12 +0000

    Use mojito-module in the Flickr source

--- sources/flickr/module.c
+++ sources/flickr/module.c
@@ -1,4 +1,4 @@
-#include "module.h"
+#include "mojito-module.h"
 #include "mojito-source-flickr.h"
 
 const gchar *
--- sources/flickr/module.h
+++
@@ -1,4 +0,0 @@
-#include <glib-object.h>
-
-const gchar *mojito_module_get_name (void);
-const GType mojito_module_get_type (void);
