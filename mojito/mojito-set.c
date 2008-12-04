75e3565451e39278e35c06415b4091cf46f685f3
Ross Burton <ross@linux.intel.com>
2008-12-04 16:28:52 +0000

    Add basic MojitoSet

---
+++ mojito/mojito-set.c
@@ -0,0 +1,93 @@
+#include "mojito-set.h"
+
+struct _MojitoSet {
+  /* Reference count */
+  volatile int ref_count;
+  /* Hash of item pointer => pointer to the senitel value */
+  GHashTable *hash;
+};
+
+static const int sentinel = 0xDECAFBAD;
+
+GType
+mojito_set_get_type (void)
+{
+  static GType type = 0;
+  if (G_UNLIKELY (type == 0)) {
+    type = g_boxed_type_register_static ("MojitoSet",
+                                         (GBoxedCopyFunc)mojito_set_ref,
+                                         (GBoxedFreeFunc)mojito_set_unref);
+  }
+  return type;
+}
+
+MojitoSet *
+mojito_set_new (GHashFunc hash_func, GEqualFunc equal_func, GDestroyNotify destroy_func)
+{
+  MojitoSet *set;
+
+  set = g_slice_new0 (MojitoSet);
+  set->ref_count = 1;
+  /* TODO: work out types and add free */
+  set->hash = g_hash_table_new_full (hash_func, equal_func, destroy_func, NULL);
+
+  return set;
+}
+
+MojitoSet *
+mojito_set_ref (MojitoSet *set)
+{
+  g_return_val_if_fail (set, NULL);
+  g_return_val_if_fail (set->ref_count > 0, NULL);
+
+  g_atomic_int_inc (&set->ref_count);
+
+  return set;
+}
+
+void
+mojito_set_unref (MojitoSet *set)
+{
+  g_return_if_fail (set);
+  g_return_if_fail (set->ref_count > 0);
+
+  if (g_atomic_int_dec_and_test (&set->ref_count)) {
+    g_hash_table_unref (set->hash);
+    g_slice_free (MojitoSet, set);
+  }
+}
+
+void
+mojito_set_add (MojitoSet *set, gpointer item)
+{
+  g_return_if_fail (set);
+  g_return_if_fail (item);
+
+  g_hash_table_insert (set->hash, item, (gpointer)&sentinel);
+}
+
+void
+mojito_set_remove (MojitoSet *set, gpointer item)
+{
+  g_return_if_fail (set);
+  g_return_if_fail (item);
+
+  g_hash_table_remove (set->hash, item);
+}
+
+gboolean
+mojito_set_has (MojitoSet *set, gpointer item)
+{
+  g_return_val_if_fail (set, FALSE);
+  g_return_val_if_fail (item, FALSE);
+
+  return g_hash_table_lookup (set->hash, item) != NULL;
+}
+
+gboolean
+mojito_set_is_empty (MojitoSet *set)
+{
+  g_return_val_if_fail (set, FALSE);
+
+  return g_hash_table_size (set->hash) == 0;
+}
---
+++ mojito/mojito-set.h
@@ -0,0 +1,30 @@
+#ifndef _MOJITO_SET
+#define _MOJITO_SET
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _MojitoSet MojitoSet;
+
+#define MOJITO_TYPE_SET mojito_set_get_type ()
+
+GType mojito_set_get_type (void);
+
+MojitoSet *mojito_set_new (GHashFunc hash_func, GEqualFunc equal_func, GDestroyNotify destroy_func);
+
+MojitoSet * mojito_set_ref (MojitoSet *set);
+
+void mojito_set_unref (MojitoSet *set);
+
+void mojito_set_add (MojitoSet *set, gpointer item);
+
+void mojito_set_remove (MojitoSet *set, gpointer item);
+
+gboolean mojito_set_has (MojitoSet *set, gpointer item);
+
+gboolean mojito_set_is_empty (MojitoSet *set);
+
+G_END_DECLS
+
+#endif /* _MOJITO_SET */
