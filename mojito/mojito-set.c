8ff535955e135764a32b6cd8629cea68895caade
Ross Burton <ross@linux.intel.com>
2008-12-08 12:18:29 +0000

    Change MojitoSet to a set of GObjects

--- mojito/mojito-set.c
+++ mojito/mojito-set.c
@@ -3,7 +3,7 @@
 struct _MojitoSet {
   /* Reference count */
   volatile int ref_count;
-  /* Hash of item pointer => pointer to the senitel value */
+  /* Hash of object pointer => pointer to the senitel value */
   GHashTable *hash;
 };
 
@@ -22,13 +22,13 @@ mojito_set_get_type (void)
 }
 
 MojitoSet *
-mojito_set_new (GHashFunc hash_func, GEqualFunc equal_func, GDestroyNotify destroy_func)
+mojito_set_new (void)
 {
   MojitoSet *set;
 
   set = g_slice_new0 (MojitoSet);
   set->ref_count = 1;
-  set->hash = g_hash_table_new_full (hash_func, equal_func, destroy_func, NULL);
+  set->hash = g_hash_table_new_full (NULL, NULL, g_object_unref, NULL);
 
   return set;
 }
@@ -56,29 +56,30 @@ mojito_set_unref (MojitoSet *set)
   }
 }
 
+/* steals the reference */
 void
-mojito_set_add (MojitoSet *set, gpointer item)
+mojito_set_add (MojitoSet *set, GObject *item)
 {
   g_return_if_fail (set);
-  g_return_if_fail (item);
+  g_return_if_fail (G_IS_OBJECT (item));
 
   g_hash_table_insert (set->hash, item, (gpointer)&sentinel);
 }
 
 void
-mojito_set_remove (MojitoSet *set, gpointer item)
+mojito_set_remove (MojitoSet *set, GObject *item)
 {
   g_return_if_fail (set);
-  g_return_if_fail (item);
+  g_return_if_fail (G_IS_OBJECT (item));
 
   g_hash_table_remove (set->hash, item);
 }
 
 gboolean
-mojito_set_has (MojitoSet *set, gpointer item)
+mojito_set_has (MojitoSet *set, GObject *item)
 {
   g_return_val_if_fail (set, FALSE);
-  g_return_val_if_fail (item, FALSE);
+  g_return_val_if_fail (G_IS_OBJECT (item), FALSE);
 
   return g_hash_table_lookup (set->hash, item) != NULL;
 }
--- mojito/mojito-set.h
+++ mojito/mojito-set.h
@@ -11,17 +11,17 @@ typedef struct _MojitoSet MojitoSet;
 
 GType mojito_set_get_type (void);
 
-MojitoSet *mojito_set_new (GHashFunc hash_func, GEqualFunc equal_func, GDestroyNotify destroy_func);
+MojitoSet *mojito_set_new (void);
 
 MojitoSet * mojito_set_ref (MojitoSet *set);
 
 void mojito_set_unref (MojitoSet *set);
 
-void mojito_set_add (MojitoSet *set, gpointer item);
+void mojito_set_add (MojitoSet *set, GObject *item);
 
-void mojito_set_remove (MojitoSet *set, gpointer item);
+void mojito_set_remove (MojitoSet *set, GObject *item);
 
-gboolean mojito_set_has (MojitoSet *set, gpointer item);
+gboolean mojito_set_has (MojitoSet *set, GObject *item);
 
 gboolean mojito_set_is_empty (MojitoSet *set);
 
