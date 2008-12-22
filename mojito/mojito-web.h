d704f3064e17ae391c3b41060ec912edbda2c736
Ross Burton <ross@linux.intel.com>
2008-12-22 15:21:09 +0000

    Remove mojito_web_cached_send, which isn't used at present

--- mojito/mojito-web.h
+++ mojito/mojito-web.h
@@ -1,6 +1,4 @@
 #include "mojito-core.h"
 #include <libsoup/soup.h>
 
-guint mojito_web_cached_send (MojitoCore *core, SoupSession *session, SoupMessage *msg);
-
 char * mojito_web_download_image (SoupSession *session, const char *url);
