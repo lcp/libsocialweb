4759c6e3e7f471840553ffbafe85bb8307acdaba
Ross Burton <ross@linux.intel.com>
2009-02-10 11:53:18 +0000

    Add lastfm to set-test-accounts.sh

--- examples/set-test-accounts.sh
+++ examples/set-test-accounts.sh
@@ -6,3 +6,6 @@ gconftool --set --type string /apps/mojito/services/flickr/user 35468147630@N01
 # Set the Twitter user to the Mojito Test user
 gconftool --set --type string /apps/mojito/services/twitter/user ross@linux.intel.com
 gconftool --set --type string /apps/mojito/services/twitter/password password
+
+# Last.fm to Ross Burton
+gconftool --set --type string /apps/mojito/services/lastfm/user rossburton
