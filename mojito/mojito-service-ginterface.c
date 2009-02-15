8e0225d6af0ae1fbafc058c5117717e991232c0c
Rob Bradford <rob@linux.intel.com>
2009-02-15 19:45:56 +0000

    Add the definition and glue code for a MojitoService DBUS API
    The DBUS API definition exports four functions:
    
    * GetLastStatus - get the last status for this service
    * GetPersonaIcon - get the icon for the logged in user
    * UpdateStatus - update the status message for this service
    * GetCapabilities - which of the above "work"

---
+++ mojito/mojito-service-ginterface.c
@@ -0,0 +1,255 @@
+#include "mojito-service-ginterface.h"
+
+#include "mojito-marshals.h"
+
+static const DBusGObjectInfo _mojito_service_iface_object_info;
+
+struct _MojitoServiceIfaceClass {
+    GTypeInterface parent_class;
+    mojito_service_iface_get_last_status_impl get_last_status;
+    mojito_service_iface_get_persona_icon_impl get_persona_icon;
+    mojito_service_iface_update_status_impl update_status;
+    mojito_service_iface_get_capabilities_impl get_capabilities;
+};
+
+static void mojito_service_iface_base_init (gpointer klass);
+
+GType
+mojito_service_iface_get_type (void)
+{
+  static GType type = 0;
+
+  if (G_UNLIKELY (type == 0))
+    {
+      static const GTypeInfo info = {
+        sizeof (MojitoServiceIfaceClass),
+        mojito_service_iface_base_init, /* base_init */
+        NULL, /* base_finalize */
+        NULL, /* class_init */
+        NULL, /* class_finalize */
+        NULL, /* class_data */
+        0,
+        0, /* n_preallocs */
+        NULL /* instance_init */
+      };
+
+      type = g_type_register_static (G_TYPE_INTERFACE,
+          "MojitoServiceIface", &info, 0);
+    }
+
+  return type;
+}
+
+/**
+ * mojito_service_iface_get_last_status_impl:
+ * @self: The object implementing this interface
+ * @context: Used to return values or throw an error
+ *
+ * The signature of an implementation of the D-Bus method
+ * GetLastStatus on interface com.intel.Mojito.Service.
+ */
+static void
+mojito_service_iface_get_last_status (MojitoServiceIface *self,
+    DBusGMethodInvocation *context)
+{
+  mojito_service_iface_get_last_status_impl impl = (MOJITO_SERVICE_IFACE_GET_CLASS (self)->get_last_status);
+
+  if (impl != NULL)
+    {
+      (impl) (self,
+        context);
+    }
+  else
+    {
+      GError e = { DBUS_GERROR, 
+           DBUS_GERROR_UNKNOWN_METHOD,
+           "Method not implemented" };
+
+      dbus_g_method_return_error (context, &e);
+    }
+}
+
+/**
+ * mojito_service_iface_implement_get_last_status:
+ * @klass: A class whose instances implement this interface
+ * @impl: A callback used to implement the GetLastStatus D-Bus method
+ *
+ * Register an implementation for the GetLastStatus method in the vtable
+ * of an implementation of this interface. To be called from
+ * the interface init function.
+ */
+void
+mojito_service_iface_implement_get_last_status (MojitoServiceIfaceClass *klass, mojito_service_iface_get_last_status_impl impl)
+{
+  klass->get_last_status = impl;
+}
+
+/**
+ * mojito_service_iface_get_persona_icon_impl:
+ * @self: The object implementing this interface
+ * @context: Used to return values or throw an error
+ *
+ * The signature of an implementation of the D-Bus method
+ * GetPersonaIcon on interface com.intel.Mojito.Service.
+ */
+static void
+mojito_service_iface_get_persona_icon (MojitoServiceIface *self,
+    DBusGMethodInvocation *context)
+{
+  mojito_service_iface_get_persona_icon_impl impl = (MOJITO_SERVICE_IFACE_GET_CLASS (self)->get_persona_icon);
+
+  if (impl != NULL)
+    {
+      (impl) (self,
+        context);
+    }
+  else
+    {
+      GError e = { DBUS_GERROR, 
+           DBUS_GERROR_UNKNOWN_METHOD,
+           "Method not implemented" };
+
+      dbus_g_method_return_error (context, &e);
+    }
+}
+
+/**
+ * mojito_service_iface_implement_get_persona_icon:
+ * @klass: A class whose instances implement this interface
+ * @impl: A callback used to implement the GetPersonaIcon D-Bus method
+ *
+ * Register an implementation for the GetPersonaIcon method in the vtable
+ * of an implementation of this interface. To be called from
+ * the interface init function.
+ */
+void
+mojito_service_iface_implement_get_persona_icon (MojitoServiceIfaceClass *klass, mojito_service_iface_get_persona_icon_impl impl)
+{
+  klass->get_persona_icon = impl;
+}
+
+/**
+ * mojito_service_iface_update_status_impl:
+ * @self: The object implementing this interface
+ * @in_status_message: const gchar * (FIXME, generate documentation)
+ * @context: Used to return values or throw an error
+ *
+ * The signature of an implementation of the D-Bus method
+ * UpdateStatus on interface com.intel.Mojito.Service.
+ */
+static void
+mojito_service_iface_update_status (MojitoServiceIface *self,
+    const gchar *in_status_message,
+    DBusGMethodInvocation *context)
+{
+  mojito_service_iface_update_status_impl impl = (MOJITO_SERVICE_IFACE_GET_CLASS (self)->update_status);
+
+  if (impl != NULL)
+    {
+      (impl) (self,
+        in_status_message,
+        context);
+    }
+  else
+    {
+      GError e = { DBUS_GERROR, 
+           DBUS_GERROR_UNKNOWN_METHOD,
+           "Method not implemented" };
+
+      dbus_g_method_return_error (context, &e);
+    }
+}
+
+/**
+ * mojito_service_iface_implement_update_status:
+ * @klass: A class whose instances implement this interface
+ * @impl: A callback used to implement the UpdateStatus D-Bus method
+ *
+ * Register an implementation for the UpdateStatus method in the vtable
+ * of an implementation of this interface. To be called from
+ * the interface init function.
+ */
+void
+mojito_service_iface_implement_update_status (MojitoServiceIfaceClass *klass, mojito_service_iface_update_status_impl impl)
+{
+  klass->update_status = impl;
+}
+
+/**
+ * mojito_service_iface_get_capabilities_impl:
+ * @self: The object implementing this interface
+ * @context: Used to return values or throw an error
+ *
+ * The signature of an implementation of the D-Bus method
+ * GetCapabilities on interface com.intel.Mojito.Service.
+ */
+static void
+mojito_service_iface_get_capabilities (MojitoServiceIface *self,
+    DBusGMethodInvocation *context)
+{
+  mojito_service_iface_get_capabilities_impl impl = (MOJITO_SERVICE_IFACE_GET_CLASS (self)->get_capabilities);
+
+  if (impl != NULL)
+    {
+      (impl) (self,
+        context);
+    }
+  else
+    {
+      GError e = { DBUS_GERROR, 
+           DBUS_GERROR_UNKNOWN_METHOD,
+           "Method not implemented" };
+
+      dbus_g_method_return_error (context, &e);
+    }
+}
+
+/**
+ * mojito_service_iface_implement_get_capabilities:
+ * @klass: A class whose instances implement this interface
+ * @impl: A callback used to implement the GetCapabilities D-Bus method
+ *
+ * Register an implementation for the GetCapabilities method in the vtable
+ * of an implementation of this interface. To be called from
+ * the interface init function.
+ */
+void
+mojito_service_iface_implement_get_capabilities (MojitoServiceIfaceClass *klass, mojito_service_iface_get_capabilities_impl impl)
+{
+  klass->get_capabilities = impl;
+}
+
+static inline void
+mojito_service_iface_base_init_once (gpointer klass G_GNUC_UNUSED)
+{
+  dbus_g_object_type_install_info (mojito_service_iface_get_type (),
+      &_mojito_service_iface_object_info);
+}
+static void
+mojito_service_iface_base_init (gpointer klass)
+{
+  static gboolean initialized = FALSE;
+
+  if (!initialized)
+    {
+      initialized = TRUE;
+      mojito_service_iface_base_init_once (klass);
+    }
+}
+static const DBusGMethodInfo _mojito_service_iface_methods[] = {
+  { (GCallback) mojito_service_iface_get_last_status, g_cclosure_marshal_VOID__POINTER, 0 },
+  { (GCallback) mojito_service_iface_get_persona_icon, g_cclosure_marshal_VOID__POINTER, 57 },
+  { (GCallback) mojito_service_iface_update_status, mojito_marshal_VOID__STRING_POINTER, 112 },
+  { (GCallback) mojito_service_iface_get_capabilities, g_cclosure_marshal_VOID__POINTER, 188 },
+};
+
+static const DBusGObjectInfo _mojito_service_iface_object_info = {
+  0,
+  _mojito_service_iface_methods,
+  4,
+"com.intel.Mojito.Service\0GetLastStatus\0A\0status\0O\0F\0N\0s\0\0com.intel.Mojito.Service\0GetPersonaIcon\0A\0uri\0O\0F\0N\0s\0\0com.intel.Mojito.Service\0UpdateStatus\0A\0status_message\0I\0s\0success\0O\0F\0N\0b\0\0com.intel.Mojito.Service\0GetCapabilities\0A\0can_get_last_status\0O\0F\0N\0b\0can_get_persona_icon\0O\0F\0N\0b\0can_update_status\0O\0F\0N\0b\0\0\0",
+"\0\0",
+"\0\0",
+};
+
+
---
+++ mojito/mojito-service-ginterface.h
@@ -0,0 +1,134 @@
+#include <glib-object.h>
+#include <dbus/dbus-glib.h>
+
+G_BEGIN_DECLS
+
+/**
+ * MojitoServiceIface:
+ *
+ * Dummy typedef representing any implementation of this interface.
+ */
+typedef struct _MojitoServiceIface MojitoServiceIface;
+
+/**
+ * MojitoServiceIfaceClass:
+ *
+ * The class of MojitoServiceIface.
+ */
+typedef struct _MojitoServiceIfaceClass MojitoServiceIfaceClass;
+
+GType mojito_service_iface_get_type (void);
+#define MOJITO_TYPE_SERVICE_IFACE \
+  (mojito_service_iface_get_type ())
+#define MOJITO_SERVICE_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), MOJITO_TYPE_SERVICE_IFACE, MojitoServiceIface))
+#define MOJITO_IS_SERVICE_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), MOJITO_TYPE_SERVICE_IFACE))
+#define MOJITO_SERVICE_IFACE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_INTERFACE((obj), MOJITO_TYPE_SERVICE_IFACE, MojitoServiceIfaceClass))
+
+
+typedef void (*mojito_service_iface_get_last_status_impl) (MojitoServiceIface *self,
+    DBusGMethodInvocation *context);
+void mojito_service_iface_implement_get_last_status (MojitoServiceIfaceClass *klass, mojito_service_iface_get_last_status_impl impl);
+/**
+ * mojito_service_iface_return_from_get_last_status:
+ * @context: The D-Bus method invocation context
+ * @out_status: const gchar * (FIXME, generate documentation)
+ *
+ * Return successfully by calling dbus_g_method_return().
+ * This inline function exists only to provide type-safety.
+ */
+static inline
+/* this comment is to stop gtkdoc realising this is static */
+void mojito_service_iface_return_from_get_last_status (DBusGMethodInvocation *context,
+    const gchar *out_status);
+static inline void
+mojito_service_iface_return_from_get_last_status (DBusGMethodInvocation *context,
+    const gchar *out_status)
+{
+  dbus_g_method_return (context,
+      out_status);
+}
+
+typedef void (*mojito_service_iface_get_persona_icon_impl) (MojitoServiceIface *self,
+    DBusGMethodInvocation *context);
+void mojito_service_iface_implement_get_persona_icon (MojitoServiceIfaceClass *klass, mojito_service_iface_get_persona_icon_impl impl);
+/**
+ * mojito_service_iface_return_from_get_persona_icon:
+ * @context: The D-Bus method invocation context
+ * @out_uri: const gchar * (FIXME, generate documentation)
+ *
+ * Return successfully by calling dbus_g_method_return().
+ * This inline function exists only to provide type-safety.
+ */
+static inline
+/* this comment is to stop gtkdoc realising this is static */
+void mojito_service_iface_return_from_get_persona_icon (DBusGMethodInvocation *context,
+    const gchar *out_uri);
+static inline void
+mojito_service_iface_return_from_get_persona_icon (DBusGMethodInvocation *context,
+    const gchar *out_uri)
+{
+  dbus_g_method_return (context,
+      out_uri);
+}
+
+typedef void (*mojito_service_iface_update_status_impl) (MojitoServiceIface *self,
+    const gchar *in_status_message,
+    DBusGMethodInvocation *context);
+void mojito_service_iface_implement_update_status (MojitoServiceIfaceClass *klass, mojito_service_iface_update_status_impl impl);
+/**
+ * mojito_service_iface_return_from_update_status:
+ * @context: The D-Bus method invocation context
+ * @out_success: gboolean  (FIXME, generate documentation)
+ *
+ * Return successfully by calling dbus_g_method_return().
+ * This inline function exists only to provide type-safety.
+ */
+static inline
+/* this comment is to stop gtkdoc realising this is static */
+void mojito_service_iface_return_from_update_status (DBusGMethodInvocation *context,
+    gboolean out_success);
+static inline void
+mojito_service_iface_return_from_update_status (DBusGMethodInvocation *context,
+    gboolean out_success)
+{
+  dbus_g_method_return (context,
+      out_success);
+}
+
+typedef void (*mojito_service_iface_get_capabilities_impl) (MojitoServiceIface *self,
+    DBusGMethodInvocation *context);
+void mojito_service_iface_implement_get_capabilities (MojitoServiceIfaceClass *klass, mojito_service_iface_get_capabilities_impl impl);
+/**
+ * mojito_service_iface_return_from_get_capabilities:
+ * @context: The D-Bus method invocation context
+ * @out_can_get_last_status: gboolean  (FIXME, generate documentation)
+ * @out_can_get_persona_icon: gboolean  (FIXME, generate documentation)
+ * @out_can_update_status: gboolean  (FIXME, generate documentation)
+ *
+ * Return successfully by calling dbus_g_method_return().
+ * This inline function exists only to provide type-safety.
+ */
+static inline
+/* this comment is to stop gtkdoc realising this is static */
+void mojito_service_iface_return_from_get_capabilities (DBusGMethodInvocation *context,
+    gboolean out_can_get_last_status,
+    gboolean out_can_get_persona_icon,
+    gboolean out_can_update_status);
+static inline void
+mojito_service_iface_return_from_get_capabilities (DBusGMethodInvocation *context,
+    gboolean out_can_get_last_status,
+    gboolean out_can_get_persona_icon,
+    gboolean out_can_update_status)
+{
+  dbus_g_method_return (context,
+      out_can_get_last_status,
+      out_can_get_persona_icon,
+      out_can_update_status);
+}
+
+
+
+G_END_DECLS
