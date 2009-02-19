ab87b43444e7af2404cd436d59b1cfd5b7e3a52c
Ross Burton <ross@linux.intel.com>
2009-02-19 14:48:18 +0000

    Change the test twitter user to an username

--- examples/set-test-accounts.sh
+++ examples/set-test-accounts.sh
@@ -4,7 +4,7 @@
 gconftool --set --type string /apps/mojito/services/flickr/user 35468147630@N01
 
 # Set the Twitter user to the Mojito Test user
-gconftool --set --type string /apps/mojito/services/twitter/user ross@linux.intel.com
+gconftool --set --type string /apps/mojito/services/twitter/user mojitotest
 gconftool --set --type string /apps/mojito/services/twitter/password password
 
 # Last.fm to Ross Burton
