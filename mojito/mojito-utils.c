59e7b6580666465e01d3f9c11d3518694e748e4e
Ross Burton <ross@linux.intel.com>
2009-03-27 15:40:57 +0000

    Add mojito_hash_string_dict to mojito-utils

--- mojito/mojito-utils.c
+++ mojito/mojito-utils.c
@@ -16,7 +16,9 @@
  * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <config.h>
 #include "mojito-utils.h"
+#include <string.h>
 #include <libsoup/soup.h>
 
 time_t
@@ -56,4 +58,33 @@ mojito_date_to_iso (const char *s)
   soup_date_free (date);
 
   return iso;
+}
+
+/**
+ * Return a hash of the contents of a string->string hash table.
+ */
+char *
+mojito_hash_string_dict (GHashTable *hash)
+{
+  GList *keys;
+  GChecksum *sum;
+  char *md5;
+
+  g_return_val_if_fail (hash, NULL);
+
+  sum = g_checksum_new (G_CHECKSUM_MD5);
+
+  keys = g_hash_table_get_keys (hash);
+  keys = g_list_sort (keys, (GCompareFunc)strcmp);
+
+  while (keys) {
+    g_checksum_update (sum, keys->data, -1);
+    g_checksum_update (sum, g_hash_table_lookup (hash, keys->data), -1);
+    keys = g_list_delete_link (keys, keys);
+  }
+
+  md5 = g_strdup (g_checksum_get_string (sum));
+  g_checksum_free (sum);
+
+  return md5;
 }
