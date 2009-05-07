82c1e594753066e211a11b4545eebc055ab8f713
Ross Burton <ross@linux.intel.com>
2009-05-07 17:57:45 +0100

    Add basic mojito-keyfob library

---
+++ mojito-keyfob/mojito-keyfob.h
@@ -0,0 +1,16 @@
+#ifndef __MOJITO_KEYFOB_H__
+#define __MOJITO_KEYFOB_H__
+
+#include <rest/oauth-proxy.h>
+
+G_BEGIN_DECLS
+
+typedef void (*MojitoKeyfobOAuthCallback) (OAuthProxy *proxy, gboolean authorised, gpointer user_data);
+
+void mojito_keyfob_oauth (OAuthProxy *proxy,
+                          MojitoKeyfobOAuthCallback callback,
+                          gpointer user_data);
+
+G_END_DECLS
+
+#endif /* __MOJITO_KEYFOB_H__ */
