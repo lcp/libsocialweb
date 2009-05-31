2ed5a3d51890a030f6eaa863708f068216d1fbff
Chen Congwu <congwu.chen@intel.com>
2009-05-31 23:56:34 +0800

    [client] Emit "item-removed" signal before removing from internal hash
    This prevents the signal being emitted with a disposed object. Fixes MB:#3079.

--- mojito-client/mojito-client-view.c
+++ mojito-client/mojito-client-view.c
@@ -184,8 +184,8 @@ _proxy_item_removed_cb (DBusGProxy  *proxy,
 
   if (item)
   {
-    g_hash_table_remove (priv->uuid_to_items, uuid);
     g_signal_emit (view, signals[ITEM_REMOVED_SIGNAL], 0, item);
+    g_hash_table_remove (priv->uuid_to_items, uuid);
   } else {
     g_critical (G_STRLOC ": Asked to remove unknown item with uuid: %s",
                 uuid);
