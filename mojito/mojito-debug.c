b0bcae15d215f4846ad5a48c5b410de3430684d9
Ross Burton <ross@linux.intel.com>
2009-09-11 17:42:55 +0100

    Add mojito-debug framework

---
+++ mojito/mojito-debug.c
@@ -0,0 +1,41 @@
+/*
+ * Mojito - social data store
+ * Copyright (C) 2009 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+#include <glib.h>
+
+#include "mojito-debug.h"
+
+guint mojito_debug_flags;
+
+void
+mojito_debug_init (void)
+{
+  static gboolean setup_done = FALSE;
+  static const GDebugKey keys[] = {
+    { "main-loop", MOJITO_DEBUG_MAIN_LOOP },
+  };
+
+  if (G_LIKELY (setup_done))
+    return;
+
+  mojito_debug_flags = g_parse_debug_string (g_getenv ("MOJITO_DEBUG"),
+                                             keys, G_N_ELEMENTS (keys));
+
+  setup_done = TRUE;
+}
---
+++ mojito/mojito-debug.h
@@ -0,0 +1,34 @@
+/*
+ * Mojito - social data store
+ * Copyright (C) 2009 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <glib.h>
+
+typedef enum {
+  MOJITO_DEBUG_MAIN_LOOP = 1 << 0
+} MojitoDebugFlags;
+
+extern guint mojito_debug_flags;
+
+#define MOJITO_DEBUG_ENABLED(category) (mojito_debug_flags & MOJITO_DEBUG_##category)
+
+#define MOJITO_DEBUG(category,x,a...)             G_STMT_START {      \
+    if (MOJITO_DEBUG_ENABLED(category))                               \
+      { g_message ("[" #category "] " G_STRLOC ": " x, ##a); }        \
+  } G_STMT_END
+
+void mojito_debug_init (void);
