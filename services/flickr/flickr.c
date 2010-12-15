aab12bf564f0f6ca79a385558638543390cec2d2
Gary Ching-Pang Lin <chingpang@gmail.com>
2010-12-15 16:36:24 +0800

    flickr: Increase ref count of service when online

--- services/flickr/flickr.c
+++ services/flickr/flickr.c
@@ -231,7 +231,7 @@ online_notify (gboolean online, gpointer user_data)
   SW_DEBUG (FLICKR, "Online: %s", online ? "yes" : "no");
 
   if (online) {
-    got_tokens_cb (priv->proxy, TRUE, service);
+    got_tokens_cb (priv->proxy, TRUE, g_object_ref (service));
   } else {
     priv->authorised = FALSE;
 
