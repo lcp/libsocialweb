85a11d5c853aaa42f50d9217624dd5563ede1cc4
Gary Ching-Pang Lin <chingpang@gmail.com>
2010-12-15 16:59:36 +0800

    twitter-item-view: Stop update timeout when the credentials are invalid
    Signed-off-by: Gary Ching-Pang Lin <chingpang@gmail.com>

--- services/twitter/twitter-item-view.c
+++ services/twitter/twitter-item-view.c
@@ -624,9 +624,23 @@ _service_capabilities_changed_cb (SwService    *service,
                                   const gchar **caps,
                                   SwItemView   *item_view)
 {
+  SwTwitterItemViewPrivate *priv = GET_PRIVATE ((SwTwitterItemView*) item_view);
+
   if (sw_service_has_cap (caps, CREDENTIALS_VALID))
   {
     twitter_item_view_refresh (item_view);
+    if (!priv->timeout_id)
+    {
+      priv->timeout_id = g_timeout_add_seconds (UPDATE_TIMEOUT,
+                                                (GSourceFunc)_update_timeout_cb,
+                                                item_view);
+    }
+  } else {
+    if (priv->timeout_id)
+    {
+      g_source_remove (priv->timeout_id);
+      priv->timeout_id = 0;
+    }
   }
 }
 
